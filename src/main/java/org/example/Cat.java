e45dea74efe62b8459665b2fa20fb40790b8afcd
catalin1989 <catalin_chira05@yahoo.com>
2023-02-03 15:49:51 +0200

    Created Dog and Cat object

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Cat {
+
+    boolean destructiveBehavior;
+
+    boolean furShed;
+}
---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Dog extends Animal{
+
+    boolean barking;
+
+    boolean peeing;
+}
