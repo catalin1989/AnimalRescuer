7fa01d42e0b03379c6506620fa1ebc2d95e731a1
catalin1989 <catalin_chira05@yahoo.com>
2023-02-06 16:21:15 +0200

    created in adaptor 2 methodes feed and activity

--- src/main/java/org/example/Adaptor.java
+++ src/main/java/org/example/Adaptor.java
@@ -4,6 +4,18 @@ public class Adaptor {
 
     int money;
 
-    String name;
+     String name;
+
+    public void feed(Animal animal,AnimalFood food){
+
+        System.out.println(name+" feed "+animal.name+" with "+food.name);
+        animal.hunger=0;
+    }
+
+    public void activity1(Activity activity, Animal animal ){
+
+        System.out.println(name+" has "+activity.name+" "+animal.name);
+        animal.happiness=10;
+    }
 
 }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,21 +8,19 @@ public class App
     public static void main( String[] args )
     {
 
-        Animal animal=new Animal("Fluffy",5,5,5,5,"Pedigree","walking");
+        Animal animal=new Animal("Fluffy",5,5,5,5,"Pedigree","walked");
 
-       Activity activity= new Activity("waling");
-
-
+       Activity activity= new Activity("walked");
 
         Doctor doctor1=new Doctor("Joe","Veterinary");
 
-
         Doctor student = new Doctor("Angie","Veterinary student");
 
-
         Adaptor person= new Adaptor();
-        person.name= "Carol";
+        person.name= "John";
         person.money=5000;
+
+
 
         Calendar d = Calendar.getInstance();
         d.set(2023,02,25);
@@ -35,8 +33,11 @@ public class App
         game.doctor= doctor1.name;
         game.dog=animal.name;
 
+    person.feed(animal,food);
+        System.out.println(animal.name+" hunger is "+animal.hunger);
 
-
+    person.activity1(activity,animal);
+        System.out.println(animal.name+" happiness is "+ animal.happiness);
 
 
 
